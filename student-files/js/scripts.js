7cd867108a82d1eeeac1df1921579e22c206a1b1
Johannes Deutsch <johannes.deutsch@gmx.at>
2022-02-06 19:38:30 +0100

    html markup

---
+++ student-files/js/scripts.js
@@ -0,0 +1,34 @@
+
+const searchContainerDiv = document.getElementsByClassName('search-container');
+searchContainerDiv.insertAdjacentHTML('beforeend', '<form action="#" method="get">');
+searchContainerDiv.insertAdjacentHTML('beforeend', '<input type="search" id="search-input" class="search-input" placeholder="Search...">');
+searchContainerDiv.insertAdjacentHTML('beforeend', '<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit"></input>');
+searchContainerDiv.insertAdjacentHTML('beforeend', ' </form>');
+
+const galleryDiv = document.getElementById('gallery');
+galleryDiv.insertAdjacentHTML('beforeend', '<div class="card">');
+galleryDiv.insertAdjacentHTML('beforeend', '<div class="card-img-container">');
+galleryDiv.insertAdjacentHTML('beforeend', '<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">');
+galleryDiv.insertAdjacentHTML('beforeend', '</div>');
+galleryDiv.insertAdjacentHTML('beforeend', '<div class="card-info-container">')
+galleryDiv.insertAdjacentHTML('beforeend', '<h3 id="name" class="card-name cap">first last</h3>');
+galleryDiv.insertAdjacentHTML('beforeend', '<p class="card-text">email</p>');
+galleryDiv.insertAdjacentHTML('beforeend', '<p class="card-text cap">city, state</p>');
+galleryDiv.insertAdjacentHTML('beforeend', '</div>');
+galleryDiv.insertAdjacentHTML('beforeend', '</div>');
+
+const body = document.getElementsByTagName('body');
+body.insertAdjacentHTML('beforeend', '<div class="modal-container">');
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
+body.insertAdjacentHTML('beforeend',
